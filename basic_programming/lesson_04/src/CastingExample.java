ec18ec36e06c64c0d5f496df80c823e4015bcf37
imiftakhov <ilias.miftakhov@yandex.ru>
2023-11-12 20:19:26 +0100

    lesson 4

---
+++ basic_programming/lesson_04/src/CastingExample.java
@@ -0,0 +1,22 @@
+public class CastingExample {
+
+    public static void main(String[] args) {
+        // Неявное приведение типов (Implicit Casting)
+        // Пример: преобразование от меньшего к большему типу данных
+        int intVal = 100;
+        long longVal = intVal; // неявное приведение из int в long
+        float floatVal = longVal; // неявное приведение из long в float
+        System.out.println("Неявное приведение (int -> long): " + longVal);
+        System.out.println("Неявное приведение (long -> float): " + floatVal);
+
+        // Явное приведение типов (Explicit Casting)
+        // Пример: преобразование от большего к меньшему типу данных
+        double doubleVal = 9.99;
+        int intValFromDouble = (int) doubleVal; // явное приведение из double в int
+        short shortValFromInt = (short) intValFromDouble; // явное приведение из int в short
+        System.out.println("Явное приведение (double -> int): " + intValFromDouble);
+        System.out.println("Явное приведение (int -> short): " + shortValFromInt);
+
+
+    }
+}
---
+++ basic_programming/lesson_04/src/NumberSystemsOperations.java
@@ -0,0 +1,33 @@
+public class NumberSystemsOperations {
+
+    public static void main(String[] args) {
+        // Двоичное число (например, 0b1101 соответствует 13 в десятичной системе)
+        int binaryNumber = 0b1101;
+
+        // Десятичное число (например, 10)
+        int decimalNumber = 10;
+
+        // Восьмеричное число (например, 0157 соответствует 111 в десятичной системе)
+        int octalNumber = 0157;
+
+        // Шестнадцатеричное число (например, 0x1A3 соответствует 419 в десятичной системе)
+        int hexNumber = 0x1A3;
+
+        // Примеры операций
+        // Сложение двоичного и десятичного числа
+        int sumBinaryDecimal = binaryNumber + decimalNumber;
+        System.out.println("Сумма двоичного и десятичного: " + sumBinaryDecimal); // 23
+
+        // Вычитание восьмеричного числа из шестнадцатеричного
+        int differenceHexOctal = hexNumber - octalNumber;
+        System.out.println("Разность шестнадцатеричного и восьмеричного: " + differenceHexOctal); // 308
+
+        // Умножение восьмеричного и десятичного числа
+        int productOctalDecimal = octalNumber * decimalNumber;
+        System.out.println("Произведение восьмеричного и десятичного: " + productOctalDecimal); // 1110
+
+        // Деление шестнадцатеричного на двоичное число
+        int divisionHexBinary = hexNumber / binaryNumber;
+        System.out.println("Частное шестнадцатеричного и двоичного: " + divisionHexBinary); // 32
+    }
+}
