f79644c63aad0847df4dcb0cc5c958ff79653492
imiftakhov <ilias.miftakhov@yandex.ru>
2023-11-13 18:12:43 +0100

    lesson 4

---
+++ basic_programming/lesson_04/src/CharExample.java
@@ -0,0 +1,35 @@
+public class CharExample {
+    public static void main(String[] args) {
+        // Конвертация char в int и обратно
+        char ch = 'A';
+        int intFromChar = ch;
+        char charFromInt = (char) intFromChar;
+
+        System.out.println("Char: " + ch);
+        System.out.println("Int from char: " + intFromChar);
+        System.out.println("Char from int: " + charFromInt);
+
+        // Создание char с использованием разных кодировок
+        char chUtf16 = '\u0041'; // UTF-16 код для 'A'
+        char chAscii = 65;       // ASCII код для 'A'
+
+        System.out.println("Char from UTF-16: " + chUtf16);
+        System.out.println("Char from ASCII: " + chAscii);
+
+        // Математические операции с char
+        char chPlusOne = (char) (ch + 1);
+        char chMinusOne = (char) (ch - 1);
+
+        System.out.println("Char plus one: " + chPlusOne);
+        System.out.println("Char minus one: " + chMinusOne);
+
+        // Дополнительные интересные операции
+        // Проверка, является ли символ буквой
+        boolean isLetter = Character.isLetter(ch);
+        // Перевод символа в верхний регистр
+        char upperCaseCh = Character.toUpperCase(chMinusOne);
+
+        System.out.println("Is letter: " + isLetter);
+        System.out.println("Upper case: " + upperCaseCh);
+    }
+}
