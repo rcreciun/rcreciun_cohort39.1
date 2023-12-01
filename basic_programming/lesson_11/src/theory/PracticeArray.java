3a3596831886b5037b48dd9b36cea5864a9b48e1
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-01 20:15:37 +0100

    lesson 12

--- basic_programming/lesson_11/src/theory/PracticeArray.java
+++ basic_programming/lesson_11/src/theory/PracticeArray.java
@@ -53,14 +53,46 @@ public class PracticeArray {
             // реализуйте логику, которая будет брать элемент из digits, конвертировать в int и перекладывать в nums.
             // Используйте для этого Integer.parseInt("23234") -> 23234 (int)
 
+            // String digit = digits[i];
+            //int num = Integer.parseInt(digit); -> Integer.parseInt(digits[i]);
+
+            //nums[i] = num; ->
+            nums[i] = Integer.parseInt(digits[i]);
         }
 
+        // сортировка
+        Arrays.sort(nums);
         // сортировака nums Arrays.sort()
         // формирования максимального числа (можно сделать путем добавления в StringBuilder и потом перевода в число)
         // от последнего элемента в nums к первому, добавляя каждый элемент в StringBuilder, либо банально прибавляя к
         // String result = "";
 
+        StringBuilder result = new StringBuilder();
+        for (int i = nums.length - 1;  i >= 0; i--) {
+            result.append(nums[i]);
+        }
 
+        // String strResult = result.toString();
 
+        if(input.equals(result.toString())) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result);
+        }
+
+        // альтернативное решение:
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        //String alternativeStr = new String(chars);
+        // StringBuilder alternative = new StringBuilder(alternativeStr);
+        //StringBuilder alternative = new StringBuilder(new String(chars));
+        StringBuilder alternative = new StringBuilder(new String(chars)).reverse();
+        // alternative = alternative;
+
+        if (input.equals(alternative.toString())) {
+            System.out.println(-1);
+        } else {
+            System.out.println(alternative);
+        }
     }
 }
