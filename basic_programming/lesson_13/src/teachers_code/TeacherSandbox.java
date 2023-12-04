bba20ab1a1f70072e52c9d8903d1fb74e2a7becf
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-04 19:35:32 +0100

    lesson 13

--- basic_programming/lesson_13/src/teachers_code/TeacherSandbox.java
+++ basic_programming/lesson_13/src/teachers_code/TeacherSandbox.java
@@ -1,4 +1,23 @@
 package teachers_code;
 
+import java.util.Arrays;
+
 public class TeacherSandbox {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 8, -8, -5, -8};
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        int secondMin = nums[1];
+        System.out.println("Второй минимальный элемент в массиве равен = " + secondMin);
+
+        // найти в массиве второй минимальный элемент с учетом того, что значение должно отличаться от минимума и
+        // последоватльность в исходном массиве менять нельзя. Запрещается также пользоваться классом Arrays
+        // (исключение Arrays.toString())
+
+
+
+    }
 }
--- basic_programming/src/Temp.java
+++ basic_programming/src/Temp.java
@@ -3,30 +3,6 @@ import java.util.Scanner;
 
 public class Temp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите пожалуйста верхний предел диапозона поиска простых чисел:");
-        int n = scanner.nextInt();
 
-
-        if (n < 2) {
-            System.out.println("введенное число некорректно");
-        } else if (n == 2) {
-            System.out.println(2 + " - простое число");
-        } else {
-            for (int i = 2; i <= n; i++) {
-                boolean isPrime = true;
-                // цикл for для проверки, что число без остатка делиться только на себя и на 1
-                for (int j = 2; j <= i / 2; j++) {
-                    if (i % j == 0) {
-                        isPrime = false;
-                        break;
-                        // если число i без остатка делиться на j, значит это не простое число
-                    }
-                }
-                if (isPrime) {
-                    System.out.println(i + " -простое число");
-                }
-            }
-        }
     }
 }
