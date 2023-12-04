89d3473ef9c0ef6f8ea7f650c65f526152f2092e
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-04 20:12:53 +0100

    lesson 13

--- basic_programming/lesson_12/src/teachers_code/TeacherSandbox.java
+++ basic_programming/lesson_12/src/teachers_code/TeacherSandbox.java
@@ -1,4 +1,19 @@
 package teachers_code;
 
 public class TeacherSandbox {
+    public static void main(String[] args) {
+        // args[0] - основа для теста
+        // args[1] - мясная начинка
+        // args[2] - соус
+        // args[3] - сыр
+
+        if (args.length != 4) {
+            System.err.println("incorrect length of array args!");
+            return;
+        }
+
+        System.out.printf(
+                "Ваша пицца будет на основе %s, с начинкой из %s, соусом %s и сыром %s. Приятного аппетита!\n",
+                args[0], args[1], args[2], args[3]);
+    }
 }
