0a39ea86f2d155e1df232ca6a4ef873e2912ab31
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-05 19:11:55 +0100

    lesson 13

--- basic_programming/consultation/src/consultation23_12_05/teachers_code/TeacherSandbox.java
+++ basic_programming/consultation/src/consultation23_12_05/teachers_code/TeacherSandbox.java
@@ -8,7 +8,6 @@ public class TeacherSandbox {
 
         // int Integer.MIN_VALUE to Integer.MAX_VALUE
         random.nextInt();
-
         for (int i = 0; i < 100; i++) {
             System.out.print(random.nextInt() + " ");
         }
@@ -19,5 +18,20 @@ public class TeacherSandbox {
         for (int i = 0; i < 100; i++) {
             System.out.printf("%.4f ", Math.random());
         }
+
+        // random int in range from 0 to 100
+        int x = random.nextInt(101); // 101 - out of the range
+
+        // decision with Math.random() in range from 0 to 100
+        int y = (int) Math.random() * 100 + 1;  // 50
+
+        // random int in range from 50 to 100
+        x = random.nextInt(50, 101); // 101 - out of the range
+
+        x = random.nextInt(3) - 1; // 0 1 2 -> -1 0 1
+
+        // range from -5 to 0 (exclude 0)
+        x = random.nextInt(-5, 0);
+        System.out.println("\n" + x);
     }
 }
---
+++ basic_programming/consultation/src/consultation23_12_05/teachers_code/TeacherSandbox2.java
@@ -0,0 +1,34 @@
+package consultation23_12_05.teachers_code;
+
+public class TeacherSandbox2 {
+    public static void main(String[] args) {
+        double[] circle = {3};
+        double[] rectangle = {3, 4};
+        double[] triangle = {3, 4, 5};
+        double[] incorrect = {3, 4, 5, 6};
+        double[] empty = {};
+
+        calculateArea(circle);
+    }
+
+    public static void calculateArea(double[] sizes) {
+        if (sizes.length == 1) {
+            // вызываем метод для подсчета площади круга
+        } else if (sizes.length == 2) {
+            // вызываем метод для подсчета площади прямоугольника
+        } else if (sizes.length == 3) {
+            // вызываем метод для подсчета площади треугольника
+        } else {
+            System.err.println("Incorrect input");
+        }
+    }
+
+    public static void calculateArea(double radius) {
+
+    }
+
+    // попробуйте решить задачу перегрузив методы
+    public static void calculateCircleArea(){
+        //...
+    }
+}
