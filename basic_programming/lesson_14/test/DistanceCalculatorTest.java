af7a47083dc85efe794841e6b1ae756321b17bf7
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-08 20:20:33 +0100

    lesson 15 unit tests

--- basic_programming/lesson_14/test/DistanceCalculatorTest.java
+++ basic_programming/lesson_14/test/DistanceCalculatorTest.java
@@ -1,7 +1,22 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import theory.DistanceCalculator;
 
-public class DistanceCalculatorTest {
+class DistanceCalculatorTest {
 
     @Test
+    public void testCalculateDistanceWithReturn() {
+        double[] point1 = {0, 0, 0};
+        double[] point2 = {1, 0, 0};
+
+        //переменная, в которой мы пишем ожидаемый
+        // результат выполнения кода
+        double expected = 1.0;
+
+        double result = DistanceCalculator.calculateDistanceWithReturn(point1, point2);
+
+        Assertions.assertEquals(expected, result);
+    }
+
 
 }
