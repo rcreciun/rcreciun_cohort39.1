1881d00f5bdf9e1575e8ed8f5ca63248a96dc5ea
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-08 20:21:17 +0100

    lesson 15 unit tests

--- basic_programming/lesson_14/test/DistanceCalculatorTest.java
+++ basic_programming/lesson_14/test/DistanceCalculatorTest.java
@@ -5,14 +5,13 @@ import theory.DistanceCalculator;
 class DistanceCalculatorTest {
 
     @Test
-    public void testCalculateDistanceWithReturn() {
+    public void testCalculateDistanceWithReturnCorrectResult() {
         double[] point1 = {0, 0, 0};
         double[] point2 = {1, 0, 0};
 
         //переменная, в которой мы пишем ожидаемый
         // результат выполнения кода
         double expected = 1.0;
-
         double result = DistanceCalculator.calculateDistanceWithReturn(point1, point2);
 
         Assertions.assertEquals(expected, result);
