59c1bcb0569d98aee85d0ec5fbb05b4b87a9b57e
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-13 19:43:11 +0100

    lesson 17 ООП, инкапсуляция

--- basic_programming/lesson_17/src/mail_service/teachers_code/MailService.java
+++ basic_programming/lesson_17/src/mail_service/teachers_code/MailService.java
@@ -1,11 +1,40 @@
 package mail_service.teachers_code;
 
+import java.util.Random;
+
 public class MailService {
-    public String login;
-    public String password;
+    private String login;
+    private String password;
 
     public MailService(String login, String password) {
         this.login = login;
         this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        // сложная логика двойной аутентификации через
+        // телефон/вторую почту и тп
+
+        // вместо сложной проверки на данном этапе
+        // используется рандомное значение boolean
+
+        if (new Random().nextBoolean()) {
+            return password;
+        } else {
+            return null;
+        }
+    }
+
+    public void setPassword(String newPassword, String oldPassword) {
+        if (oldPassword.equals(password)) {
+            password = newPassword;
+            System.out.println("Вы успешно поменяли свой пароль");
+        } else {
+            System.out.println("Некорректное значение старого пароля, значение пароля не было изменено");
+        }
     }
 }
--- basic_programming/lesson_17/src/teachers_code/TeacherSandbox.java
+++ basic_programming/lesson_17/src/teachers_code/TeacherSandbox.java
@@ -1,8 +1,26 @@
 package teachers_code;
 
-import java.util.Arrays;
-import java.util.Scanner;
+
+import mail_service.teachers_code.MailService;
 
 public class TeacherSandbox {
+    public static void main(String[] args) {
+        MailService ilyasMail = new MailService("ilyas@mail.com",
+                "1233");
 
+        MailService anotherMail = new MailService("some_login@mail.com", "134234");
+
+        // ..
+
+        System.out.println("ilyasMail.login = " + ilyasMail.getLogin());
+        System.out.println("ilyasMail.password = " + ilyasMail.getPassword());
+
+        System.out.println("anotherMail.getLogin() = " + anotherMail.getLogin());
+        System.out.println("anotherMail.getPassword() = " + anotherMail.getPassword());
+
+        ilyasMail.setPassword("4567", "1233");
+
+        System.out.println("ilyasMail.getClass() = " + ilyasMail.getClass().getSimpleName());
+        System.out.println("anotherMail.getClass() = " + anotherMail.getClass().getSimpleName());
+    }
 }
