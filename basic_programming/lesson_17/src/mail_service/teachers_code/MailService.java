258be1a8d8000494e9baeefa9eba0c169ef69dd6
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-13 19:11:23 +0100

    lesson 17 ООП, инкапсуляция

---
+++ basic_programming/lesson_17/src/mail_service/teachers_code/MailService.java
@@ -0,0 +1,11 @@
+package mail_service.teachers_code;
+
+public class MailService {
+    public String login;
+    public String password;
+
+    public MailService(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
