3e85c47063b01666262f85bcdf986dfd6225ddd1
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-15 19:39:31 +0100

    lesson 18 ООП, наследование

--- basic_programming/lesson_18/src/theory/MedicineCart.java
+++ basic_programming/lesson_18/src/theory/MedicineCart.java
@@ -17,6 +17,15 @@ public class MedicineCart {
 
     public MedicineCart(String name) {
         this.name = name;
+
+        // с учетом того, что при регистрации мы узнаем лишь одно значение - name,
+        // все остальные поля будут проинициализированы значениями по умолчанию, что равносильно:
+        // (не обязательно это писать)
+        this.age = 0;
+        this.gender = null;
+        this.weight = 0.0;
+        this.isPregnant = false;
+
     }
 
     // конструктор, который позволяет указать при регистрации только имя и возраст
@@ -24,6 +33,14 @@ public class MedicineCart {
         this.age = age;
         this.name = name;
     }
+
+    // конструктор для анонимного использования нашего медицинского сервиса.
+    // Для его использования необходимо указать пол и возраст. Тк это теже самые типы данных, что в конструкторе
+    //  MedicineCart(String name, int age), единственное что остается - поменять эти ТИПЫ данных местами, то есть
+    //  MedicineCart(String gender, int age) - ВЫЗОВЕТ ОШИБКУ!!!
+    //  MedicineCart(int age, String gender) - будет корректно соседствовать с уже существующим конструктором,
+    //  потому что с точки зрения JVM они разные: MedicineCart(String, int) != MedicineCart(int, String)
+
 
     // конструктор, который позволяет заполнить сразу все данные
     public MedicineCart(int age, double weight, String name, String gender, boolean isPregnant) {
