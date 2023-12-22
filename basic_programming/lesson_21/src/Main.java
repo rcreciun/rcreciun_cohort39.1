92d02f0d93889c398af50145d815399b32cdee27
imiftakhov <ilias.miftakhov@yandex.ru>
2023-12-22 19:19:52 +0100

    lesson 21 GIT

--- basic_programming/lesson_21/src/Main.java
+++ basic_programming/lesson_21/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     // changes
     public static void main(String[] args) {
         System.out.println("main branch changes");
+
+
     }
 }
---
+++ basic_programming/lesson_21/src/teachers_code/TeacherSandbox.java
@@ -0,0 +1,7 @@
+package teachers_code;
+
+public class TeacherSandbox {
+    public static void main(String[] args) {
+        System.out.println("Hello, fork!");
+    }
+}
